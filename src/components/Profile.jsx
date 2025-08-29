f3f79ff29fc78d03da94faa966af0a0afc5f45ed
marjorygc <marjorymarjory18@gmail.com>
2025-08-29 11:57:29 -0300

    aula 2

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Profile(){
+    return(
+        <div className="profile">
+          <img
+            src="https://github.com/liberato-utfpr.png"
+            alt="avatar do Rafael"
+            className="profile-avatar"
+          />
+          <h1 className="profile-name">Rafael Liberato</h1>
+          <p className="profile-bio">Desenvolvedor React JS</p>
+        </div>
+    );
+}
